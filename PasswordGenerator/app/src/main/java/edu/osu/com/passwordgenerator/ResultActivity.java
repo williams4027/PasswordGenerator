3b5c51a3bee7630fcb348caf3dc53f5cd0c55b65
williams4027 <williams.4027@gmail.com>
2015-03-25 16:20:58 -0400

    Added initial activities

--- PasswordGenerator/app/src/main/java/edu/osu/com/passwordgenerator/ResultActivity.java
+++ PasswordGenerator/app/src/main/java/edu/osu/com/passwordgenerator/ResultActivity.java
@@ -1,45 +1,24 @@
 package edu.osu.com.passwordgenerator;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class HomeActivity extends ActionBarActivity {
-
-    Context thisContext = this;
+public class ResultActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        TextView label = (TextView) findViewById(R.id.homeLabel);
-        Log.d("DEBUG", label.getText().toString());
-        label.setText("Howdy");
-
-        Button submitButton = (Button)findViewById(R.id.button);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent configIntent = new Intent(thisContext, PasswordConfigurationActivity.class);
-                startActivity(configIntent);
-            }
-        });
+        setContentView(R.layout.activity_result);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 
